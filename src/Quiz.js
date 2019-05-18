7be859815623e3b82d37e9c89edb1092448c60c8
David <taisun93@gmail.com>
2019-05-18 14:19:04 -0500

    Added Quiz.js

--- src/Quiz.js
+++ src/Quiz.js
@@ -5,11 +5,11 @@ let quizData = require('./quiz_data.json')
 class Quiz extends Component{
   constructor(props) {
     super(props)
-    this.state = {quiz_position: 1}
+    this.state = {quiz_position: 2}
   }
   render(){
     return (
-      <div><div className='QuizQuestion'>{quizData.quiz_questions[0].instruction_text}</div></div>
+      <div><div className='QuizQuestion'>{quizData.quiz_questions[1].instruction_text}</div></div>
     )
   }
 }
